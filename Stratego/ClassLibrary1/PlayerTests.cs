9f85e00964b90d969efd0391322d15f268e0ef4c
millerns <millerns@rose-hulman.edu>
2013-04-11 01:40:01 -0400

    Added Player and PlayerTests.

---
+++ Stratego/ClassLibrary1/PlayerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Stratego;
+
+namespace StrategoTesting
+{
+    [TestFixture()]
+    class PlayerTests
+    {
+        [Test()]
+        public void TestThatPlayerInitializesWithProperValues()
+        {
+           
+        }
+
+        [Test()]
+        public void TestThatNameSets()
+        {
+        }
+    }
+}
--- Stratego/Stratego/Player.cs
+++ Stratego/Stratego/Player.cs
@@ -8,5 +8,38 @@ namespace Stratego
 {
     class Player
     {
+        private Int16 pieceCount;
+        private String name;
+        private Boolean hasColors;
+
+        public Player()
+        {
+        }
+
+        public Player(String name)
+        {
+        }
+
+        public void setName(String name)
+        {
+            
+        }
+
+        public Int16 getPieceCount()
+        {
+            return -1;
+        }
+
+        public String getName()
+        {
+            return null;
+        }
+
+        public Boolean getHasColors()
+        {
+            return false;
+        }
+
+
     }
 }
