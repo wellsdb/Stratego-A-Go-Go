525c91d551546feb314a04020abd4d79804cc5fe
millerns <millerns@rose-hulman.edu>
2013-04-18 01:40:30 -0400

    Created classes within Network. Created outline of tests for these classes. Project files were modified accordingly.

--- Stratego/Network/Client.cs
+++ Stratego/Network/Client.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace Network
 {
-    public abstract class Networker
+    class Client
     {
-        
-    }   
+    }
 }
---
+++ Stratego/Testing/NetworkTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Network;
+using NUnit.Framework;
+
+namespace Testing
+{
+    [TestFixture()]
+    class NetworkTests
+    {
+
+        [Test()]
+        public void TestThatClientSendsMessage()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatServerRecievesMessage()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatGameSenderEncodesGameProperly()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatGameRecieverDecodesGameProperly()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatGameSenderStartsAClient()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatGameRecieverStartsAServer()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatClientStarts()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatServerStarts()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatSendingPortsCanBeChanged()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatRecievingPortsCanBeChanged()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatNetworkControllerGetsGameData()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatNetworkControllerReturnsGameData()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatNetworkControllerAttemptsToSendData()
+        {
+
+        }
+
+        [Test()]
+        public void TestThatNetworkControllerAttemptsToRecieveData()
+        {
+
+        }
+
+        
+
+    }
+}
