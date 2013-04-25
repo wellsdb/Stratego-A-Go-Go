d81f382a8bc3fdd8d89bef60de73c1cdaa59bc5a
millerns <millerns@rose-hulman.edu>
2013-04-25 02:22:38 -0400

    Some additions to NetworkController and merged saving/loading into Game.

--- Stratego/Network/NetworkController.cs
+++ Stratego/Network/NetworkController.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Net.Sockets;
+using System.Threading;
 
 namespace Network
 {
@@ -15,6 +16,9 @@ namespace Network
         private Port sendPort;
         private Port recievePort;
         private IPAddress ip;
+        private Server server;
+        private ByteContainer cont;
+        private Byte[] data;
 
         public NetworkController(Port sendPort, Port recievePort, String ip)
         {
@@ -26,36 +30,92 @@ namespace Network
         public Boolean SendString(String message)
         {
             //encode string
+            Byte[] encoded = new Converter().StringToByte(message);
             
             //create client
+            Client client = new Client(new IPEndPoint(ip, (int)sendPort));
 
             //wait for client to send
+            Boolean sent = client.Send(encoded);
 
             //  success
+            if (sent)
+                return true;
 
             //  fail
+            if (!sent)
+                return false;
 
             //  timeout
             
             return false;
         }
 
+        private void StartServer()
+        {
+            this.server.Start();
+        }
+
+        private void StartServer(Server server)
+        {
+            server.Start();
+        }
+
+        private void GetDataFromContainer()
+        {
+            this.data =  this.cont.ReadData();
+        }
+
+        private Byte[] GetDataFromContainer(ByteContainer cont)
+        {
+            return cont.ReadData();
+        }
+
         public String RecieveString()
         {
+            ByteContainer cont = new ByteContainer();
             //create server
+           this.server = new Server(recievePort, cont);
 
-            //wait for server to recieve
+            Thread serverThread = new Thread(new ThreadStart(StartServer));
+            //Thread readThread = new Thread(new ThreadStart(GetDataFromContainer));
 
-            //  error
+            serverThread.Start();
+
+            while (!serverThread.IsAlive);
+
+            Thread.Sleep(5000);
+
+            serverThread.Abort();
+            serverThread.Join();
+
+            Byte[] data = cont.ReadData();
+
+            //try
+            //{
+            //    serverThread.Start();
+            //    readThread.Start();
+            //}
+            //catch (ThreadStateException e)
+            //{
+            //    Console.WriteLine(e);  // Display text of exception
+            //}
+            //catch (ThreadInterruptedException e)
+            //{
+            //    Console.WriteLine(e);  // This exception means that the thread
+            //    // was interrupted during a Wait
+            //}
+
+            //server.Stop();
+            // error
+            if (data == null)
+                return null;
+
+            //success
+            return new Converter().ByteToString(data);
 
             //  timeout
 
-            //  success
-
-            //decode bytes
-
-            //return string
-            
             return null;
         }
 
--- Stratego/Stratego/Game.cs
+++ Stratego/Stratego/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Stratego
 {
@@ -37,6 +38,11 @@ namespace Stratego
             return this.board;
         }
 
+        public void setBoard(Board board)
+        {
+            this.board = board;
+        }
+
         public String getPlayerName(Int16 player)
         {
             if (player == 1)
@@ -69,11 +75,21 @@ namespace Stratego
             return this.turnCount;
         }
 
+        public void setTurnCount(Int16 count)
+        {
+            this.turnCount = count;
+        }
+
         public Piece.Team getCurrentTurn()
         {
             return this.currentTeam;
         }
-        
+
+        public void setCurrentTurn(Piece.Team current)
+        {
+            this.currentTeam = current;
+        }
+
         public void startGame()
         {
             this.turnCount = 1;
@@ -165,6 +181,190 @@ namespace Stratego
             {
                 this.currentTeam = Piece.Team.red;
                 this.turnCount++;
+            }
+        }
+        public void saveFile(String fileName)
+        {
+            StreamWriter file = new StreamWriter(fileName);
+            //Basic stuff, turn count etc.
+            file.WriteLine(this.getTurnCount());
+            file.WriteLine(this.getCurrentTurn().ToString());
+
+            //Player One info
+            file.WriteLine(this.getPlayerName(1));
+            //Player Two info
+            file.WriteLine(this.getPlayerName(2));
+            //Board info
+            Board board = this.getBoard();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (board.getPiece(i, j) != null)
+                    {
+                        file.WriteLine(board.getPiece(i, j).getRank().ToString() + board.getPiece(i, j).getTeam().ToString());
+                    }
+                    else
+                    {
+                        file.WriteLine(board.getCell(i, j).getTerrain().ToString());
+                    }
+
+                }
+
+            }
+
+            file.Close();
+        }
+
+        public void loadFile(String fileName)
+        {
+            StreamReader file = null;
+            try
+            {
+                file = new StreamReader(fileName);
+                this.setTurnCount(short.Parse(file.ReadLine()));
+                String current = file.ReadLine();
+                if (current == "red")
+                {
+                    this.setCurrentTurn(Piece.Team.red);
+                }
+                else if (current == "blue")
+                {
+                    this.setCurrentTurn(Piece.Team.blue);
+                }
+                else
+                {
+                    this.setCurrentTurn(Piece.Team.none);
+                }
+
+                this.setPlayerName(1, file.ReadLine());
+                this.setPlayerName(2, file.ReadLine());
+
+                Board temp = new Board();
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        String square = file.ReadLine();
+
+                        if (square == "Land")
+                        {
+                            temp.getCell(i, j).setTerrain(Cell.Terrain.Land);
+                        }
+                        else if (square == "Lake")
+                        {
+                            temp.getCell(i, j).setTerrain(Cell.Terrain.Land);
+                        }
+                        else if (square == "flag red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.flag), i, j);
+                        }
+                        else if (square == "flag blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.flag), i, j);
+                        }
+                        else if (square == "spy red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.spy), i, j);
+                        }
+                        else if (square == "spy blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.spy), i, j);
+                        }
+                        else if (square == "scout red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.scout), i, j);
+                        }
+                        else if (square == "scout blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.scout), i, j);
+                        }
+                        else if (square == "miner red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.miner), i, j);
+                        }
+                        else if (square == "miner blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.miner), i, j);
+                        }
+                        else if (square == "sergeant red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.sergeant), i, j);
+                        }
+                        else if (square == "sergeant blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.sergeant), i, j);
+                        }
+                        else if (square == "lieutenant red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.lieutenant), i, j);
+                        }
+                        else if (square == "lieutenant blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.lieutenant), i, j);
+                        }
+                        else if (square == "captain red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.captain), i, j);
+                        }
+                        else if (square == "captain blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.captain), i, j);
+                        }
+                        else if (square == "major red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.major), i, j);
+                        }
+                        else if (square == "major blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.major), i, j);
+                        }
+                        else if (square == "colonel red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.colonel), i, j);
+                        }
+                        else if (square == "colonel blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.colonel), i, j);
+                        }
+                        else if (square == "general red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.general), i, j);
+                        }
+                        else if (square == "general blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.general), i, j);
+                        }
+                        else if (square == "marshal red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.marshal), i, j);
+                        }
+                        else if (square == "marshal blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.marshal), i, j);
+                        }
+                        else if (square == "bomb red")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.red, Piece.Rank.bomb), i, j);
+                        }
+                        else if (square == "bomb blue")
+                        {
+                            temp.placePiece(new Piece(Piece.Team.blue, Piece.Rank.bomb), i, j);
+                        }
+                    }
+                }
+
+                this.setBoard(temp);
+
+            }
+            catch (FileNotFoundException e)
+            {
+                throw new FileNotFoundException("Yeah something's up here", e);
+            }
+            finally
+            {
+                if (file != null)
+                    file.Close();
             }
         }
         
